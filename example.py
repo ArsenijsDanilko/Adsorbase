09ad736de223648aedcc7174b6bf3e535fa95641
diacoal <aurele.dubois16@gmail.com>
2025-04-28 14:19:39 +0200

    Adapted df names

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ from sklearn.decomposition import PCA
 import plotly.express as px
 import molplotly
 
-csv_file = Path("/workspaces/Adsorbase/adsorbents.csv")
+csv_file = Path("/home/aurele/Projects/Adsorbase/adsorbents.csv")
 df2 = pd.read_csv(csv_file, sep= ",")
 df2.head()
 
@@ -17,9 +17,9 @@ df2.head()
 fig_pca = px.scatter(df2, # data to pull from
                      x="Pore volume", # column name withx-axis
                      y="Adsorption capacity", # column name withy-axis
-                     color=' "Type of Adsorbent"', # color by application
+                     color='Type of Adsorbent', # color by application
                      title='Adsorbents', # title of the plot
-                     labels={'color': ' "Type of Adsorbent"'},  # label for the color legend
+                     labels={'color': 'Type of Adsorbent'},  # label for the color legend
                      width=1200,
                      height=800)
 
@@ -29,8 +29,10 @@ app_pca = molplotly.add_molecules(fig=fig_pca,  # figure to populate
                                   smiles_col='BET Surface Area',  # column with SMILES
                                   title_col='Name',  # column to take the name for the molecule when hovering over it
                                   caption_cols=['Conditions P', 'Conditions T'], # columns to take the additional captions from
-                                  color_col=' "Type of Adsorbent"', # column to color by
+                                  color_col='Type of Adsorbent', # column to color by
                                   show_coords=False)
 
 # show the plot: don't use the same port several times as it will overwrite the previous one
-app_pca.run(jupyter_mode="tab", port=8404)
+app_pca.run(port=8404)
+
+#jupyter_mode="tab", 
