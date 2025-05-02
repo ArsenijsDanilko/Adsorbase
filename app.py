8d0b56a9c21c96d4e28ced4321296e548fe607b4
diacoal <aurele.dubois16@gmail.com>
2025-05-02 13:14:24 +0200

    Added hover data, axis choice

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ cwd = Path.cwd()
 df = pd.read_csv(cwd / "adsorbents.csv")
 app = Dash(__name__)
 
-hover_options = [
+data_options = [
     "Type of Adsorbent",
     "BET Surface Area",
     "Pore volume",
@@ -15,31 +15,75 @@ hover_options = [
     "Conditions T",
     "Conditions P",
 ]
+data_options = list(df.head(1))
 app.layout = html.Div(
     [
-        html.H4("Select hover data:"),
-        dcc.Dropdown(
-            id="hover-dropdown",
-            options=[{"label": col, "value": col} for col in hover_options],
-            value=["Pore Volume", "Adsorption capacity"],
-            multi=True,
+        # Axis dropdowns in a horizontal row
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Label("Select x-axis:"),
+                        dcc.Dropdown(
+                            id="x-axis",
+                            options=[
+                                {"label": col, "value": col} for col in data_options
+                            ],
+                            value="Adsorption capacity",
+                            multi=False,
+                        ),
+                    ],
+                    style={"flex": 1, "margin-right": "10px"},
+                ),
+                html.Div(
+                    [
+                        html.Label("Select y-axis:"),
+                        dcc.Dropdown(
+                            id="y-axis",
+                            options=[
+                                {"label": col, "value": col} for col in data_options
+                            ],
+                            value="Pore volume",
+                            multi=False,
+                        ),
+                    ],
+                    style={"flex": 1},
+                ),
+            ],
+            style={"display": "flex", "margin-bottom": "20px"},
         ),
+        # Graph below the dropdowns
         dcc.Graph(id="scatter-plot"),
+        # Hover data selector
+        html.Div(
+            [
+                html.Label("Select hover data:"),
+                dcc.Dropdown(
+                    id="hover-dropdown",
+                    options=[{"label": col, "value": col} for col in data_options],
+                    value=[],
+                    multi=True,
+                ),
+            ]
+        ),
     ]
 )
 
 
-@app.callback(Output("scatter-plot", "figure"), Input("hover-dropdown", "value"))
-def update_scatter(selected_hover_data):
+@app.callback(
+    Output("scatter-plot", "figure"),
+    Input("x-axis", "value"),
+    Input("y-axis", "value"),
+    Input("hover-dropdown", "value"),
+)
+def update_scatter(x_axis, y_axis, selected_hover_data):
     fig = px.scatter(
-        df,  # data to pull from
-        x="Pore volume",  # column name withx-axis
-        y="Adsorption capacity",  # column name withy-axis
-        size="BET Surface Area",
-        size_max=20,
-        color="Type of Adsorbent",  # color by application
-        title="Adsorbents",  # title of the plot
-        labels={"color": "Type of Adsorbent"},  # label for the color legend
+        data_frame=df,
+        x=x_axis,
+        y=y_axis,
+        color="Type of Adsorbent",
+        title="Adsorbents",
+        labels={"color": "Type of Adsorbent"},
         hover_data=selected_hover_data,
         hover_name="Name",
         width=1000,
