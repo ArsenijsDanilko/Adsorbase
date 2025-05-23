7aa2707c0f0f37edb013490982cfcb1890c81ee1
diacoal <aurele.dubois16@gmail.com>
2025-05-23 16:47:35 +0200

    fixed range sliders not updating

--- src/adsorbase/layout.py
+++ src/adsorbase/layout.py
@@ -1,32 +1,29 @@
-from dash import html, dcc, dash_table
+from dash import html, dcc
 from math import floor, ceil
-from adsorbase.utils import load_df, axis_options, ROOT_PATH
+from adsorbase.utils import load_df, current_data, axis_options
 import dash_bootstrap_components as dbc
-import pandas as pd 
-import os 
 from dash_bootstrap_templates import ThemeSwitchAIO
 
-csv_file = ROOT_PATH / 'data/adsorbents.csv'
 df = load_df()
-custom_path = ROOT_PATH / 'data/custom.csv'
+filtered_df = current_data()
 
 color_mode_switch = html.Span([
-    dbc.Label(className="fa fa-moon", html_for="color-mode-switch"),
+    dbc.Label(className='fa fa-moon', html_for='color-mode-switch'),
     dbc.Switch(
-        id="color-mode-switch",
+        id='color-mode-switch',
         value=False,
-        className="d-inline-block ms-1",
+        className='d-inline-block ms-1',
         persistence=True
     ),
-    dbc.Label(className="fa fa-sun", html_for="color-mode-switch"),
+    dbc.Label(className='fa fa-sun', html_for='color-mode-switch'),
 ])
 
 theme_switch = html.Div([
     ThemeSwitchAIO(
-        aio_id="theme",
+        aio_id='theme',
         themes=[dbc.themes.BOOTSTRAP, dbc.themes.DARKLY]
     )],
-    style={"position": "absolute", "top": "20px", 'right': "20px"}
+    style={'position': 'absolute', 'top': '20px', 'right': '20px'}
 )
 
 title = html.H1(
@@ -48,60 +45,54 @@ subtitle = html.H3(
         'fontSize': '1.8em',
         'fontStyle': 'italic',
         'letterSpacing': '1px',
-        'marginBottom' : '20px'  
+        'marginBottom': '20px'
     }
 )
 
 
 x_dropdown = html.Div([
-    html.Label("Select x-axis:"),
+    html.Label('Select x-axis:'),
     dcc.Dropdown(
         {col: col for col in axis_options},
         'Pore volume [cm³/g]',
         id='xaxis-column',
         style={'color': 'black'}
     ),
-], 
-    id='xaxis-container', 
-    style={'width': '48%', 
+],
+    id='xaxis-container',
+    style={'width': '48%',
            'display': 'inline-block',
-           'marginBottom' : '20px',
-           'marginTop' : '20px',
-           'marginLeft' : '20px'  
-        }
+           'marginBottom': '20px',
+           'marginTop': '20px',
+           'marginLeft': '20px'
+           }
 )
 
 y_dropdown = html.Div([
-    html.Label("Select y-axis:"),
+    html.Label('Select y-axis:'),
     dcc.Dropdown(
         {col: col for col in axis_options},
         'BET Surface Area [m²/g]',
         id='yaxis-column',
         style={'color': 'black'}
     ),
-], 
-    id='yaxis-container', 
-    style={'width': '48%', 
+],
+    id='yaxis-container',
+    style={'width': '48%',
            'display': 'inline-block',
-           'marginBottom' : '20px',
-           'marginTop' : '20px',
-           'marginLeft' : '20px'  
-        }
+           'marginBottom': '20px',
+           'marginTop': '20px',
+           'marginLeft': '20px'
+           }
 )
 
-def current_data() -> pd.DataFrame:
-        if os.path.exists(custom_path):
-            df = pd.read_csv(custom_path)
-        else:
-            df = pd.read_csv(csv_file)
-        return df
-
-temp_slider_text = html.Label('Select the right temperature conditions [K]',
-                              style= {'marginLeft' : '20px'})
-filtered_df = current_data()
+temp_slider_text = html.Label(
+    'Select the right temperature conditions [K]',
+    style={'marginLeft': '20px'}
+)
 temp_slider = dcc.RangeSlider(
-    floor(filtered_df['Conditions T [K]'].min()/10)*10,
-    ceil(filtered_df['Conditions T [K]'].max()/10)*10,
+    min = floor(filtered_df['Conditions T [K]'].min()/10)*10,
+    max = ceil(filtered_df['Conditions T [K]'].max()/10)*10,
     step=None,
     updatemode='drag',
     tooltip={'placement': 'bottom', 'always_visible': True},
@@ -109,13 +100,14 @@ temp_slider = dcc.RangeSlider(
     id='Temp-slider'
 )
 
-pressure_slider_text = html.Label('Select the right pressure conditions [bar]',
-                                  style= {'marginLeft' : '20px',
-                                          'marginTop' : '5px'})
-filtered_df = current_data()
+pressure_slider_text = html.Label(
+    'Select the right pressure conditions [bar]',
+    style={'marginLeft': '20px', 'marginTop': '5px'}
+)
+
 pressure_slider = dcc.RangeSlider(
-    floor(filtered_df['Conditions P [bar]'].min()),
-    ceil(filtered_df['Conditions P [bar]'].max()),
+    min = floor(df['Conditions P [bar]'].min()/10)*10,
+    max = ceil(df['Conditions P [bar]'].max()/10)*10,
     step=None,
     updatemode='drag',
     tooltip={'placement': 'bottom', 'always_visible': True},
@@ -131,42 +123,49 @@ hover_dropdown = html.Div([
         value=[],
         multi=True,
         style={
-            'width': '100%',         
+            'width': '100%',
             'color': 'black',
             'justify-content': 'center',
-            'marginLeft' : '5px'          
+            'marginLeft': '5px'
         }
     )
 ], style={
-    'width': '60%',                 
-    'display': 'inline-block' ,
-    'marginBottom' : '20px',
-    'marginTop' : '20px',
-    'marginLeft' : '20px'   
+    'width': '60%',
+    'display': 'inline-block',
+    'marginBottom': '20px',
+    'marginTop': '20px',
+    'marginLeft': '20px'
 })
 
 graph = dcc.Graph(id='indicator-graphic',
-                  style={'height':'90vh'})
+                  style={'height': '90vh'})
 shown_count = html.Div(
     id='point-count',
-    style={'marginBottom': '20px', 
-           'marginTop' : '20px', 
-           'marginLeft' : '20px',
+    style={'marginBottom': '20px',
+           'marginTop': '20px',
+           'marginLeft': '20px',
            'fontWeight': 'bold'}
 )
 
 input_title = html.H3('Extending the database',
-                      style={'marginLeft' : '20px'})
+                      style={'marginLeft': '20px'})
 input_prompt = html.P('Note: use periods as decimal separators, not commas',
-                      style={'marginLeft' : '20px'})
+                      style={'marginLeft': '20px'})
 input_fields = [
-    dcc.Input(id='input-name', type='text', placeholder='Name', style={'marginBottom' : '20px','marginLeft' : '20px', 'width' : '18%'}),
-    dcc.Input(id='input-type', type='text', placeholder='Type of adsorbent', style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'}),
-    dcc.Input(id='input-BET', type='number', placeholder='BET Surface Area [m²/g]',style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'}),
-    dcc.Input(id='input-Pore', type='number', placeholder='Pore volume [cm³/g]',style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'}),
-    dcc.Input(id='input-Ads', type='number', placeholder='Adsorption capacity [mmol/g]',style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'}),
-    dcc.Input(id='input-T', type='number', placeholder='Conditions T [K]',style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'}),
-    dcc.Input(id='input-P', type='number', placeholder='Conditions P [bar]',style={'marginBottom' : '20px','marginLeft' : '20px','width' : '18%'})
+    dcc.Input(id='input-name', type='text', placeholder='Name',
+              style={'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-type', type='text', placeholder='Type of adsorbent',
+              style={'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-BET', type='number', placeholder='BET Surface Area [m²/g]', style={
+              'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-Pore', type='number', placeholder='Pore volume [cm³/g]', style={
+              'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-Ads', type='number', placeholder='Adsorption capacity [mmol/g]', style={
+              'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-T', type='number', placeholder='Conditions T [K]', style={
+              'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'}),
+    dcc.Input(id='input-P', type='number', placeholder='Conditions P [bar]', style={
+              'marginBottom': '20px', 'marginLeft': '20px', 'width': '18%'})
 ]
 
 add_button = dbc.Button(
@@ -183,7 +182,8 @@ actualize_button = dbc.Button(
     style={'marginLeft': '20px'}
 )
 
-add_output = html.Div(id='output', style={'color': 'green', 'marginLeft': '20px'})
+add_output = html.Div(id='output', style={
+                      'color': 'green', 'marginLeft': '20px'})
 
 filtered_table_title = html.H3(
     'Filtered Adsorbents Table',
@@ -202,7 +202,7 @@ export_button = dbc.Button(
     n_clicks=0,
     style={'marginTop': '20px',
            'marginBottom': '20px',
-           'marginLeft' : '20px'}
+           'marginLeft': '20px'}
 )
 
 download = dcc.Download(id='download-dataframe-csv')
--- src/adsorbase/utils.py
+++ src/adsorbase/utils.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from pathlib import Path
+import os
 
 ROOT_PATH = Path(__file__).resolve().parents[2]
+custom_path = ROOT_PATH / 'data/custom.csv'
+csv_file = ROOT_PATH / 'data/adsorbents.csv'
+
 
 column_titles = []
 
@@ -14,6 +18,13 @@ axis_options = column_titles[2:5]
 
 def load_df(filepath: Path | str = Path(ROOT_PATH / 'data/adsorbents.csv')) -> pd.DataFrame:
     return pd.read_csv(filepath, sep=',')
+
+def current_data() -> pd.DataFrame:
+        if os.path.exists(custom_path):
+            df = pd.read_csv(custom_path)
+        else:
+            df = pd.read_csv(csv_file)
+        return df
 
 
 if __name__ == '__main__':
